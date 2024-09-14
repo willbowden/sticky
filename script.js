1e6e032f2145ea657eac14274845e602f92ec085
willbowden <willbowden03@yahoo.co.uk>
2024-09-14 10:42:35 +0800

    Dragging notes

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ class Notes {
         y: 50,
       }
     };
+    this._dragging = false;
+
+    let container = document.querySelector(this._containerClass);
+    container.addEventListener("mousedown", this.handleDrag.bind(this));
 
     this.populateNotes();
   }
@@ -24,12 +28,46 @@ class Notes {
       noteElem.innerHTML = `${note.content}`;
       noteElem.style.left = note.x + "px";
       noteElem.style.top = note.y + "px";
-      
+
       container.append(noteElem);
     }
   }
+
+  handleDrag(event) {
+    if (this._dragging) return;
+
+    let target = event.target.closest("div.note");
+
+    if (!target) return;
+
+    this._dragging = true;
+
+    let offsetX = event.clientX - target.offsetLeft;
+    let offsetY = event.clientY - target.offsetTop;
+
+    const onMouseMove = (event) => {
+      if (!this._dragging) return;
+
+      target.style.left = event.clientX - offsetX + "px";
+      target.style.top = event.clientY - offsetY + "px";
+
+    }
+
+    const onMouseUp = () => {
+      if (!this._dragging) return;
+
+      this._dragging = false;
+
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    }
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+  }
+
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const n = new Notes(".notespace");
 }, false);
